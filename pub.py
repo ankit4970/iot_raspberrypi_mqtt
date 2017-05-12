470c9f5e6bd76564c6eed3335ef01a4dc070cf8d
Ankit Gandhi <ankit4970@gmail.com>
2017-05-12 03:43:39 +0000

    Added receive data handing and commit to mysql

--- pub.py
+++ pub.py
@@ -3,11 +3,12 @@ import paho.mqtt.client as mqtt
 import time
 
 mqttc = mqtt.Client()
-mqttc.connect("localhost", 1883)
-mqttc.publish("hello/world", "Hello, World!")
+mqttc.connect("iot.eclipse.org", 1883)
+mqttc.publish("cmpe297/sensor", "Hello, World!")
 
 while True:
-	mqttc.publish("hello/world", "Hello, World!")
+	buf="27.83,728.3,34.3,05/17/2017"
+	mqttc.publish("cmpe297/sensor", buf)
 	time.sleep(1)
 
 mqttc.loop_forever() 									#timeout = 2s
--- sub.py
+++ sub.py
@@ -2,17 +2,36 @@
 
 import paho.mqtt.client as mqtt
 import MySQLdb
+import time
 
-
+# Define Variables
+MQTT_BROKER = "iot.eclipse.org"
+MQTT_PORT = 1883
+MQTT_KEEPALIVE_INTERVAL = 60
+MQTT_TOPIC = "cmpe297/sensor"
+temperature =0.0
+lat = 37.336082
+lon = -121.882572
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe("hello/world")
+    client.subscribe(MQTT_TOPIC)
 
 def on_message(client, userdata, msg):
-	print(msg.payload.decode())
+	#print(msg)
+	var = (msg.payload.decode())
+	print(var)
+	var1=var.split(',')
+	for words in var1:
+		print(words)
+	datetime = time.strftime("%m/%d/%Y")
+	localtime = time.asctime( time.localtime(time.time()) )
+	print("Local current time :", localtime)
 	try:
+		lat =(float)(var1[0])
+		lon=(float)(var1[1])
+		temp =(float)(var1[2])
 		# Execute the SQL command
-		cursor.execute(sql)
+		cursor.execute("INSERT INTO sensordata (lat,lon,temperature,datetime) values (%f,%f,%f,'%s')" %(lat,lon,temp,localtime))
    		# Commit your changes in the database
 		db.commit()
 	except:
@@ -20,15 +39,15 @@ def on_message(client, userdata, msg):
 		db.rollback()
 
 # Open database connection
-db = MySQLdb.connect("localhost","root","root","mysql" )
+db = MySQLdb.connect("localhost","root","root","sensor" )
 
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
-sql = """INSERT INTO sensorData (lat,lon,temperature,datetime) values (37.3382080,-121.8863290,5,4)"""
+sql = "INSERT INTO sensordata (lat,lon,temperature,datetime) values (%f,%f,%f,'%s')" %(lat,-121.8863290,29.89,"05/10/2017")
 
 client = mqtt.Client()
-client.connect("localhost", 1883, 60)
+client.connect(MQTT_BROKER,MQTT_PORT,MQTT_KEEPALIVE_INTERVAL )
 
 client.on_connect = on_connect
 client.on_message = on_message
