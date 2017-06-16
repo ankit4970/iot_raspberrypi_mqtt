76a0fda7b064dea70f0d0d10b3f7a55aabbb38a0
Ankit Gandhi <ankit4970@gmail.com>
2017-06-15 20:33:18 -0700

    modified pub.py

--- pub.py
+++ pub.py
@@ -4,11 +4,11 @@ import time
 
 mqttc = mqtt.Client()
 mqttc.connect("iot.eclipse.org", 1883)
-mqttc.publish("cmpe297/sensor", "Hello, World!")
+mqttc.publish("iot/sensor", "Hello, World!")
 
 while True:
 	buf="27.83,728.3,34.3,05/17/2017"
-	mqttc.publish("cmpe297/sensor", buf)
+	mqttc.publish("iot/sensor", buf)
 	time.sleep(1)
 
 mqttc.loop_forever() 									#timeout = 2s
